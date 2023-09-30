54df63823171d4d95696656a130c138209c79fa5
zilberberg <zil.eran@gmail.com>
2023-09-30 13:17:01 +0300

    search field

--- src/pages/users/usersList/UsersList.jsx
+++ src/pages/users/usersList/UsersList.jsx
@@ -4,12 +4,19 @@ import AddButton from '../../../components/AddButton';
 import styles from '../users.module.css';
 import { useEffect, useState } from 'react';
 import PrimaryButton from '../../../components/PrimaryButton';
+import InputField from '../../../components/InputField';
 
 const PAGE_AMOUNT = 10;
 
 function UsersList({usersData, handleAdd, errors, handleUserEdit, onDelete}) {
   const [ currentIndex, setCurrentIndex ] = useState(0);
-  const [ visableUsers, setVisableUsers ] = useState(usersData.slice(0, 10));
+  const [ visableUsers, setVisableUsers ] = useState(usersData.slice(0, PAGE_AMOUNT));
+  const [ searchValue, setSearchValue ] = useState("");
+  const [ filteredResults, setFilteredResults ] = useState(visableUsers);
+
+  useEffect(() => {
+    setFilteredResults(visableUsers.filter(user => user.name.toLowerCase().includes(searchValue.toLowerCase())));
+  }, [visableUsers, searchValue])
 
   useEffect(() => {
     setVisableUsers(usersData.slice(currentIndex, currentIndex+PAGE_AMOUNT));
@@ -29,7 +36,15 @@ function UsersList({usersData, handleAdd, errors, handleUserEdit, onDelete}) {
         />
       </div>
       <div className={styles.usersListContent}>
-        {visableUsers?.map((user, index) => {
+        <InputField
+          name={"name"}
+          value={searchValue}
+          onChangehandler={(id, name, value) => setSearchValue(value)}
+          placeholder={"Search user name"}
+        />
+      </div>
+      <div className={styles.usersListContent}>
+        {filteredResults?.map((user, index) => {
           const userErrors = errors.filter(error => error.id == user.id);
           return <UserRow key={index} user={user} onEdit={handleUserEdit} onDelete={onDelete} errors={userErrors}/>
         })}
